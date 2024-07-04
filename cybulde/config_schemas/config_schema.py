07374f703cffa9199cacf24d109bf9802c2b40a7
dbmello <danbmello@gmail.com>
2024-07-04 16:55:52 -0300

    Added functionality to initialize DVC Storage

--- cybulde/config_schemas/config_schema.py
+++ cybulde/config_schemas/config_schema.py
@@ -1,12 +1,15 @@
 from hydra.core.config_store import ConfigStore
 from pydantic.dataclasses import dataclass
+# from dataclasses import field
 
 
 @dataclass
 class Config:
-    helle: str = "world"
+    dvc_remote_name: str = "gcs-storage"
+    dvc_remote_url: str = "gs://cybulde/data/raw"
 
 
 def setup_config() -> None:
     cs = ConfigStore.instance()
     cs.store(name="config_schema", node=Config)
+
--- cybulde/utils/data_utils.py
+++ cybulde/utils/data_utils.py
@@ -11,7 +11,6 @@ def is_dvc_initialized() -> bool:
 
 # Function to initialize DVC
 def initialize_dvc() -> None:
-    
     # If DVC is initialized, prevents to initialize again
     if is_dvc_initialized():
         DATA_UTIL_LOGGER.info("DVC is already initialize")
@@ -29,4 +28,19 @@ def initialize_dvc() -> None:
     run_shell_command("git add .dvc")
     # Commit our changes
     run_shell_command("git commit -m 'Initialized DVC'")
+
+
+# Function to initialize DVC Storage
+def initialize_dvc_storage(dvc_remote_name: str, dvc_remote_url: str) -> None:
+    # Check if DVC Storage was already initialize
+    if not run_shell_command("dvc remote list"):
+        DATA_UTIL_LOGGER.info("Initializing DVC storage...")
+        # Initializing DVC remote repository
+        run_shell_command(f"dvc remote add -d {dvc_remote_name} {dvc_remote_url}")
+        # Add DVC config to GitHub again (That's because the above command change .dvc/config)
+        run_shell_command("git add .dvc/config")
+        # Commit changes
+        run_shell_command(f"git commit -m 'Configured remote storage at: {dvc_remote_url}'")
+    else:
+        DATA_UTIL_LOGGER.info("DVC storage was already initialized...")
 
